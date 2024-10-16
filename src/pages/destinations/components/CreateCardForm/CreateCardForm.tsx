37dcaa0845ccdaa37f43b6f50872622802210dab
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2024-10-16 15:52:06 +0400

    transform create card form to controlled

--- src/pages/destinations/components/CreateCardForm/CreateCardForm.tsx
+++ src/pages/destinations/components/CreateCardForm/CreateCardForm.tsx
@@ -1,14 +1,39 @@
-import { CreateCardFormProps } from "@/types/types";
+import { CardFormStateObj, CreateCardFormProps } from "@/types/types";
 import styles from "./CreateCardForm.module.css";
+import { ChangeEvent, FormEvent, useState } from "react";
 
+const formInitialObj = {
+  countryName: "",
+  population: "",
+  capitalCity: "",
+  area: "",
+  currency: "",
+  flagURL: "",
+};
 const CreateCardForm: React.FC<CreateCardFormProps> = ({ onSubmit }) => {
+  const [cardFormstate, setCardFormState] =
+    useState<CardFormStateObj>(formInitialObj);
+  const { countryName, population, capitalCity, area, currency, flagURL } =
+    cardFormstate;
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setCardFormState((prevCardFormSate) => {
+      return { ...prevCardFormSate, [name]: value };
+    });
+  };
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    onSubmit(event, cardFormstate);
+  };
   return (
-    <form className={styles.country_form} onSubmit={onSubmit}>
+    <form className={styles.country_form} onSubmit={handleSubmit}>
       <div>
         <div>
           <div>
             <label htmlFor="country-name">Country Name</label>
             <input
+              value={countryName}
+              onChange={handleChange}
               type="text"
               name="countryName"
               id="country-name"
@@ -19,6 +44,8 @@ const CreateCardForm: React.FC<CreateCardFormProps> = ({ onSubmit }) => {
           <div>
             <label htmlFor="country-population">Population</label>
             <input
+              value={population}
+              onChange={handleChange}
               type="number"
               name="population"
               id="country-population"
@@ -29,6 +56,8 @@ const CreateCardForm: React.FC<CreateCardFormProps> = ({ onSubmit }) => {
           <div>
             <label htmlFor="capital-city">Capital City</label>
             <input
+              value={capitalCity}
+              onChange={handleChange}
               type="text"
               name="capitalCity"
               id="capital-city"
@@ -41,6 +70,8 @@ const CreateCardForm: React.FC<CreateCardFormProps> = ({ onSubmit }) => {
           <div>
             <label htmlFor="country-area">Country Area km²</label>
             <input
+              value={area}
+              onChange={handleChange}
               type="number"
               name="area"
               id="country-area"
@@ -51,6 +82,8 @@ const CreateCardForm: React.FC<CreateCardFormProps> = ({ onSubmit }) => {
           <div>
             <label htmlFor="country-currency">Currency</label>
             <input
+              value={currency}
+              onChange={handleChange}
               type="text"
               name="currency"
               id="country-currency"
@@ -61,6 +94,8 @@ const CreateCardForm: React.FC<CreateCardFormProps> = ({ onSubmit }) => {
           <div>
             <label htmlFor="country-flag-url">Flag URL</label>
             <input
+              value={flagURL}
+              onChange={handleChange}
               type="url"
               name="flagURL"
               id="country-flag-url"
